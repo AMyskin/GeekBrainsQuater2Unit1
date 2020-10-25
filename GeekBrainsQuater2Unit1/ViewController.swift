9cfce592696a2a2d2873948cfbae629de7355cce
Alexander Myskin <amyskin@yandex.ru>
2020-10-25 15:24:18 +0300

    Initial Commit

---
+++ GeekBrainsQuater2Unit1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeekBrainsQuater2Unit1
+//
+//  Created by Alexander Myskin on 25.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
